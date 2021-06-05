dbcf41e3fa4612d71729886956153436da4094bc
_pedrocarvalho96 <developerpedrocarvalho@gmail.com>
2021-06-05 20:44:50 -0300

    start project

---
+++ app/src/main/java/app/devpedrocarvalho/firstunittest/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.devpedrocarvalho.firstunittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
